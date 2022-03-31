3714331dcdea86496d5aae46b2238b31826a11ac
karikarshivani <karikarshivani@gmail.com>
2022-03-31 17:10:43 -0400

    Updated the function to replicate lambda trigger logic
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- hdfToSplunkViaS3/handler.js
+++ hdfToSplunkViaS3/handler.js
@@ -1,12 +1,47 @@
 'use strict';
 
-module.exports.hello = function main(event, context) {
+const aws = require('aws-sdk')
+const my_state_machine_arn = os.environ['MY_STATE_MACHINE_ARN']
+
+// module.exports.hello = function main(event, context, callback) {
+//   const stepFunctions = new aws.StepFunctions()
+//   stepfunctions.startExecution(params, (err, data) => {
+//     if (err) {
+//     console.log(err);
+//     };
+//     }
+
+module.exports.hello = (event, context, callback) => {
+  console.log(event)
+
+  const params = { // Takes my env arn
+    stateMachineArn: my_state_machine_arn,
+    input: JSON.stringify({})
+  };
+
+  const stepfunctions = new aws.StepFunctions()
   
-  if (event.Records === null) {
-    //context.fail('Error: Event has no records.')
-    console.log(JSON.stringify(event))
-    return
-  }
-  console.log(JSON.stringify(event))
-  return
+  event['Records'].forEach(record => {
+    stepfunctions.startExecution(params, (err, data) => {
+      if (err) {
+      console.log(err);
+      const response = {
+          statusCode: 500,
+          body: JSON.stringify({
+          message: 'There was an error'
+          })
+      };
+      callback(null, response);
+      } else {
+      console.log(data);
+      const response = {
+          statusCode: 200,
+          body: JSON.stringify({
+          message: 'Step function worked'
+          })
+      };
+      callback(null, response);
+      }
+    })
+  });
 }
