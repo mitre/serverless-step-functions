c7eee9f34506081d2708e968f96baf5fbdcbc9f0
Camden Moors <cmoors@mitre.org>
2022-04-13 14:43:42 -0400

    Lint

--- hdfToSplunkViaS3/handler.js
+++ hdfToSplunkViaS3/handler.js
@@ -6,14 +6,14 @@ const my_state_machine_arn = process.env.STATE_MACHINE_ARN;
 
 module.exports.stateMachineTrigger = async (event, context, callback) => {
   const stepfunctions = new aws.StepFunctions();
-  
+
   const bucket = event.Records[0].s3.bucket.name;
   console.log("Bucket " + bucket);
   const key = decodeURIComponent(
     event.Records[0].s3.object.key.replace(/\+/g, " ")
-    );
+  );
   console.log("key " + key);
-    
+
   // READ PARAMS
   const fileParameters = {
     Bucket: bucket,
@@ -27,27 +27,27 @@ module.exports.stateMachineTrigger = async (event, context, callback) => {
 
   try {
     // event['Records'].forEach(record => {
-      await stepfunctions.startExecution(params, async (err, data) => {
-        console.log("STARTING STATE MACHINE WITH PARAM INPUT " + params.input);
-        if (err) {
+    await stepfunctions.startExecution(params, async (err, data) => {
+      console.log("STARTING STATE MACHINE WITH PARAM INPUT " + params.input);
+      if (err) {
         console.log(err);
         const response = {
-            statusCode: 500,
-            body: JSON.stringify({
+          statusCode: 500,
+          body: JSON.stringify({
             message: 'There was an error'
-            })
+          })
         };
         callback(null, response);
-        } else {
+      } else {
         console.log(data);
         const response = {
-            statusCode: 200,
-            body: JSON.stringify({
+          statusCode: 200,
+          body: JSON.stringify({
             message: 'Step function worked'
-            })
+          })
         };
         callback(null, response);
-        }
+      }
     }).promise();
   } catch (e) {
     console.log(e)
